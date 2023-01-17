fc116e4194b9578a79e0252fbde70213fb40f0e3
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-01-17 11:35:36 -0300

    Feat: Function to create a new user created

---
+++ src/main/java/com/tweteroo_spring/api/controller/loginController.java
@@ -0,0 +1,18 @@
+package com.tweteroo_spring.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo_spring.api.dto.UserDTO;
+
+@RestController
+@RequestMapping("/api/auth/sign-up")
+public class loginController {
+  
+  @PostMapping
+  public void createUser(@RequestBody UserDTO req){
+    System.out.println(req);
+  }
+}
---
+++ src/main/java/com/tweteroo_spring/api/dto/UserDTO.java
@@ -0,0 +1,5 @@
+package com.tweteroo_spring.api.dto;
+
+public record UserDTO(String _username, String _avatar) {
+
+} 
