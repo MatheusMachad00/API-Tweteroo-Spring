dfa8f4e79559ab3b82466c312a260a57077ccb17
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-01-18 11:22:21 -0300

    Feat: tweet model and user model created

--- src/main/java/com/tweteroo_spring/api/controller/TweetsController.java
+++ src/main/java/com/tweteroo_spring/api/controller/TweetsController.java
@@ -1,18 +1,25 @@
 package com.tweteroo_spring.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo_spring.api.dto.TweetDTO;
+import com.tweteroo_spring.api.model.Tweet;
 
 @RestController
-@RequestMapping("/tweets")
+@RequestMapping("/api/tweets")
 public class TweetsController {
 
   @PostMapping
   public void createTweet(@RequestBody TweetDTO req) {
     System.out.println(req);
   }
+
+  /* @GetMapping
+  public List<Tweet> getTweets() {
+    return (Arrays.asList());
+  } */
 }
---
+++ src/main/java/com/tweteroo_spring/api/model/Tweet.java
@@ -0,0 +1,26 @@
+package com.tweteroo_spring.api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Tweet {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 30, nullable = false)
+  private String username;
+
+  @Column(length = 500, nullable = false)
+  private String avatar;
+
+  @Column(length = 500, nullable = false)
+  private String text;
+
+}
