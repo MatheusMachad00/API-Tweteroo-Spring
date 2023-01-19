d269cf8c223ade7e5850adcfbce2cf0a233b8768
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-01-19 15:58:08 -0300

    Feat: Route to get tweets by username created

--- src/main/java/com/tweteroo_spring/api/repository/TweetRepository.java
+++ src/main/java/com/tweteroo_spring/api/repository/TweetRepository.java
@@ -1,9 +1,13 @@
 package com.tweteroo_spring.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tweteroo_spring.api.model.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-  
+
+  List<Tweet> findByUsername(String username);
+
 }
